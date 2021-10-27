6080b379bf9e496dfaf973560c49961373c2b430
adagustafik <ada.gustafik@gmail.com>
2021-10-27 15:38:21 +0200

    Added line-through on todo list items

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ $('button').click (() => {
     $('ul').append("<li>" + $('input:text').val() +"</li>");
     $('input').val("");
 });
+
+
+$('ul').click(function(event) {
+    $(event.target).css('text-decoration-line','line-through');
+});
+
+
+
+/*
+$('li').click(function(event) {
+    $(event.target).css('text-decoration-line','line-through');
+});
+*/
